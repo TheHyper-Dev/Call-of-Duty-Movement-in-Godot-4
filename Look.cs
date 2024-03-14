fd1ecc87d6d3a741b74456442194a71f2e16a0f1
TheHyper-Dev <32967925+TheHyper-Dev@users.noreply.github.com>
2024-03-14 17:55:01 +0300

    Update Look.cs

--- Look.cs
+++ Look.cs
@@ -4,87 +4,77 @@ using Godot;
 [GlobalClass]
 public sealed partial class Look : Resource
 {
-    public PlayerController controller;
+    public Player player;
     public Node3D node;
+    [Export] public bool Active = true;
 
-    [Export] public bool look_enabled = true;
+    [Export] public Vector2 Sensitivity = Vector2.One;
+    [Export] public Vector2 Rot = Vector2.Zero;
 
-    [Export]
-    public Vector2
-    Sensitivity = Vector2.One,
-    Rot = Vector2.Zero;
-
-    [Export]
-    public float
-    height_offset = 1.8f,
-    cam_lerp_speed = 40f;
-
-    [Export] public bool lerp_enabled = false;
-    public void Init(PlayerController controller)
+    [Export] public float height_offset = 1.8f;
+    [Export] public float cam_lerp_speed = 15f;
+    [Export] public CameraSway cameraSway;
+    public void Init(Player player)
     {
-        this.controller = controller;
-        node = (Node3D)controller.GetNode(controller.look_path);
+        this.player = player;
+        player.SetNode(out node, look_path);
+        cameraSway = cameraSway == null ? new() : (CameraSway)cameraSway.Duplicate();
+        cameraSway.Init(this);
+
     }
-    public void UnhandledInput(InputEvent input)
+    public void UnhandledInputMouseMotion(InputEventMouseMotion mouse_motion)
     {
-        if (!look_enabled) return;
-        if (input is InputEventMouseMotion mouse_motion)
-        {
-            Rot -= mouse_motion.Relative * Sensitivity;
-            if (Mathf.Abs(Rot.X) >= 360f)
-                Rot.X = 0f;
-            if (Mathf.Abs(Rot.Y) > 89.5f)
-                Rot.Y = Mathf.Sign(Rot.Y) * 89.5f;
-        }
+        if (!Active) return;
+        Rot -= mouse_motion.Relative * Sensitivity;
+        if (Mathf.Abs(Rot.X) >= 360f)
+            Rot.X = 0f;
+        if (Mathf.Abs(Rot.Y) > 89.5f)
+            Rot.Y = Mathf.Sign(Rot.Y) * 89.5f;
+
+        player.move.move_direction = Movement.CalculateDiagonalMoveDirection(in player.look.Rot.X, in player.cmd.move.X, in player.cmd.move.Y);
     }
-    Vector2 smoothedRot = Vector2.Zero;
+
+    Vector2 FinalRot = Vector2.Zero;
     [Export(PropertyHint.Range, "0.001, 1")] public float Smoothing = 0.2f; // Min value does the most smoothing, Max Value does the least (or none at all if set to 1f)
 
-    public void Update(in float dt)
+    public void Update()
     {
-        //node.GlobalPosition = node.GlobalPosition.Lerp(new Vector3(controller.Position.X, controller.Position.Y + height_offset, controller.Position.Z), dt * cam_lerp_speed);
-        if (!lerp_enabled)
-        {
-            Vector3 player_vel = controller.move.velocity;
 
-            float Y = controller.GlobalPosition.Y + height_offset + controller.move.current_stance_look_height;
-            node.GlobalTranslate(dt * player_vel);
+        cameraSway.Update();
 
-            Vector3 cur_pos = node.GlobalPosition;
-            cur_pos.Y = Y;
-            node.GlobalPosition = cur_pos;
-        }
-        else
-        {
-            Vector3 controller_pos = controller.GlobalPosition;
-            Vector3 look_pos = node.GlobalPosition;
+        Vector3 look_pos = node.GlobalPosition;
 
-            float lerp_speed_delta = dt * cam_lerp_speed;
-            float lerp_speed_hor = lerp_speed_delta * controller.move.horSpeed;
-            float lerp_speed_Y = lerp_speed_delta * Math.Abs(controller.move.velocity.Y);
+        float lerp_speed_hor = player.move.horSpeed;
 
-            float stance_change_delta = dt * controller.move.stance_change_speed;
-            if (lerp_speed_Y < stance_change_delta)
-                lerp_speed_Y = stance_change_delta;
+        if (lerp_speed_hor < player.move.target_speed)// in case the speed becomes higher than the target speed
+            lerp_speed_hor = player.move.target_speed;
 
-            look_pos.X = Mathf.MoveToward(look_pos.X, controller_pos.X, lerp_speed_hor);
-            look_pos.Y = Mathf.MoveToward(look_pos.Y, controller_pos.Y + height_offset + controller.move.current_stance_look_height, lerp_speed_Y); // smooth ascending/descending
-            look_pos.Z = Mathf.MoveToward(look_pos.Z, controller_pos.Z, lerp_speed_hor);
-            node.GlobalPosition = look_pos;
-        }
-        smoothedRot.X = LerpAngleDegrees(smoothedRot.X, Rot.X, Smoothing);
-        smoothedRot.Y = LerpAngleDegrees(smoothedRot.Y, Rot.Y, Smoothing);
+        lerp_speed_hor = lerp_speed_hor * cam_lerp_speed * Game.Delta;
+        if (lerp_speed_hor > 1f)
+            lerp_speed_hor = 1f;
 
-        node.GlobalRotationDegrees = new Vector3(smoothedRot.Y, smoothedRot.X, 0f);
+        float lerp_speed_Y = Math.Abs(player.move.velocity.Y);
+
+        if (lerp_speed_Y < player.move.target_speed)
+            lerp_speed_Y = player.move.target_speed;
+            
+        lerp_speed_Y = lerp_speed_Y * cam_lerp_speed * Game.Delta;
+        if (lerp_speed_Y > 1f)
+            lerp_speed_Y = 1f;
+
+        look_pos.X = Mathf.Lerp(look_pos.X, player.move.Position.X, lerp_speed_hor) + cameraSway.position.X;
+        look_pos.Y = Mathf.Lerp(look_pos.Y, player.move.Position.Y + height_offset + player.move.current_stance_look_height, lerp_speed_Y) + cameraSway.position.Y; // smooth ascending/descending plus camera sway
+        GD.Print(lerp_speed_Y);
+        look_pos.Z = Mathf.Lerp(look_pos.Z, player.move.Position.Z, lerp_speed_hor);
+        node.GlobalPosition = look_pos;
+
+        FinalRot.X = Utilities.LerpAngleDegrees(in FinalRot.X, in Rot.X, in Smoothing) + cameraSway.rotation_degrees.X;
+        FinalRot.Y = Utilities.LerpAngleDegrees(in FinalRot.Y, in Rot.Y, in Smoothing) + cameraSway.rotation_degrees.Y;
+
+        node.GlobalRotationDegrees = new Vector3(FinalRot.Y, FinalRot.X, 0f);
 
     }
-    public static float AngleDifference(float from, float to)
-    {
-        float num = (to - from) % 360f;
-        return 2f * num % 360f - num;
-    }
-    public static float LerpAngleDegrees(float from, float to, float weight)
-    {
-        return from + AngleDifference(from, to) * weight;
-    }
+
+    [ExportGroup("NodePaths")]
+    [Export] NodePath look_path;
 }
